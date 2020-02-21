4f767e3be5b1f625fca279f11cbcf7589812e01a
Aaliyah <ortizaa00@gmail.com>
2020-02-20 19:17:30 -0500

    added to main

--- main.cpp
+++ main.cpp
@@ -6,8 +6,19 @@ int main() {
 
     std::cout << "Cadence: " << rollingThing.getCadence() << std::endl;
     std::cout << "Speed: " << rollingThing.getSpeed() << std::endl;
-    std::cout << "Gear: " << rollingThing.getGear() << std::endl;
+    std::cout << "Gear: " << rollingThing.getGear() << "\n" << std::endl;
 
+    rollingThing.changeCadence(1);
+    rollingThing.speedUp(1);
+    rollingThing.changeGear(2);
+
+    std::cout << "Cadence: " << rollingThing.getCadence() << std::endl;
+    std::cout << "Speed: " << rollingThing.getSpeed() << std::endl;
+    std::cout << "Gear: " << rollingThing.getGear() << "\n" << std::endl;
+
+    rollingThing.applyBrakes(1);
+    std::cout << "Slow down by: 1" << std::endl;
+    std::cout << "Speed: " << rollingThing.getSpeed() << std::endl;
 
     return 0;
 }
