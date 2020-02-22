f83de847459d9343a1e9427f16366dee2e9deba6
Aaliyah <ortizaa00@gmail.com>
2020-02-21 19:30:46 -0500

    added to main

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main() {
     int slowDown; //used for user
     int goFast;
+    int gearChange, cadenceChange;
 
     Bicycle rollingThing(0, 0, 1);
     ThreeSpeedBicycle gearThing(0); //this must check if the user is allow to change gears
@@ -12,9 +13,16 @@ int main() {
     std::cout << "Speed: " << rollingThing.getSpeed() << std::endl;
     std::cout << "Gear: " << rollingThing.getGear() << "\n" << std::endl;
 
-    rollingThing.changeCadence(1);
-    rollingThing.speedUp(1);
-    rollingThing.changeGear(2);
+    std::cout << "Cadence Change: ";
+    std::cin >> cadenceChange;
+    std::cout << "Speed Up By: ";
+    std::cin >> goFast;
+    std::cout << "Switch Gear: ";
+    std::cin >> gearChange;
+
+    rollingThing.changeCadence(cadenceChange);
+    rollingThing.changeGear(gearChange);
+    rollingThing.speedUp(goFast);
 
     std::cout << "Cadence: " << rollingThing.getCadence() << std::endl;
     std::cout << "Speed: " << rollingThing.getSpeed() << std::endl;
